6b99df42de0c8f19975f3a820ce939258d613afe
Theodore Ando <theodoreando99@gmail.com>
2016-01-19 15:01:16 -0600

    Trying out google maps API

---
+++ geolocationTracking.js
@@ -0,0 +1,25 @@
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+function showPosition(position) {
+    var x = document.getElementById("demo");
+    x.innerHTML = "Latitude: " + position.coords.latitude +
+        "<br>Longitude: " + position.coords.longitude;
+}
+
+function initMap() {
+    // Create a map object and specify the DOM element for display.
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        scrollwheel: false,
+        zoom: 8
+    });
+}
+
+$(document).ready(getLocation);
+initMap();
